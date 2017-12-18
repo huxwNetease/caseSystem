9b0090f1126cfa97c08212b136764b31c8948acf
huxwNetease <shadow_workspace@126.com>
2017-12-18 19:21:24 +0800

    Initial commit

---
+++ caseSource/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CasesourceConfig(AppConfig):
+    name = 'caseSource'
---
+++ caseSource/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path, include
+
+from login import views
+
+urlpatterns = [
+    url(r'^index',views.index),
+    url(r'^words',views.words),
+]
